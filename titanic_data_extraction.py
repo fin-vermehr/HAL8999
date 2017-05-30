4da3bb385d8f067b90a0777ecf5d5477df10e6f3
finvermehr <fin.vermehr@gmail.com>
2017-05-30 16:03:50 -0400

    s

--- titanic_data_extraction.py
+++ titanic_data_extraction.py
@@ -3,4 +3,4 @@ import csv
 with open("/Users/finvermehr/Downloads/train.csv", "rb") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=",", quotechar='|')
     for column in spamreader:
-        print (column)
+        print (column[0])
