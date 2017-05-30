747e81a6660390f3e1936637d39ec7dbbf31a91d
finvermehr <fin.vermehr@gmail.com>
2017-05-30 16:24:36 -0400

    d

--- titanic_data_extraction.py
+++ titanic_data_extraction.py
@@ -2,5 +2,12 @@ import csv
 
 with open("/Users/finvermehr/Downloads/train.csv", "rb") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=",", quotechar='|')
+    dic = dict()
     for column in spamreader:
-        print (column[0])
+        local_dic = {"Survived": column[1]}
+
+
+#        local_dic = {"Survived": column[1], "Pclass": column[2], "Name": (column[3] + column[4]), "Sex": column[5], "Age": column[6], "SibSp": column[7], "Parch": column[8], "Ticket": column[9], "Fare": column[10], "Cabin": column[11], "Embarked": column[12]}
+#        dic[column[0]] = local_dic
+
+# print(dic['1']["Name"])
