8788e9fcf1617d3d349eeb2849f71b7825385d82
finvermehr <fin.vermehr@gmail.com>
2017-05-30 22:59:59 -0400

    I preprocessed the data, and saving it as a csv file for visual aid

--- train_data_extraction_01f.py
+++ train_data_extraction_01f.py
@@ -9,6 +9,14 @@ df = pd.read_csv('/Users/finvermehr/Documents/GitHub/HAL8999/train.csv')
 df['Cabin'] = df["Cabin"].fillna(0)
 df['Embarked'] = df["Embarked"].fillna("Q")
 
-cols_to_transform = ['Sex', 'Embarked']
+df.loc[df['Name'].str.contains('Dr.'), 'Title'] = "Dr."
+df.loc[df['Name'].str.contains('Mr.'), 'Title'] = "Mr."
+df.loc[df['Name'].str.contains('Mrs.'), 'Title'] = "Mrs."
+df.loc[df['Name'].str.contains('Miss.'), 'Title'] = "Miss."
+df.loc[df['Name'].str.contains('Master.'), 'Title'] = "Master."
+df.loc[df['Name'].str.contains('Rev.'), 'Title'] = "Rev."
+
+cols_to_transform = ['Sex', 'Embarked', "Title"]
 df_with_dummies = pd.get_dummies(df, columns=cols_to_transform)
 print(df_with_dummies)
+df_with_dummies.to_csv("/Users/finvermehr/Documents/GitHub/HAL8999/preprocessed_data.csv", sep='\t', encoding='utf-8')
